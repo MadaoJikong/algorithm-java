89b41c9983e68428674d10c50d8a7c98d4aa3e11
chengjunchao <jccheng@zenking.cc>
2020-12-08 10:42:38 +0800

    修改注释

--- src/com/jikong/design/structural/adapter/Main.java
+++ src/com/jikong/design/structural/adapter/Main.java
@@ -4,7 +4,7 @@ package com.jikong.design.structural.adapter;
  * @author chengjunchao
  * @title: Main
  * @projectName algorithm-java
- * @description: 适配器执行
+ * @description: 适配器执行  适配器由原来的Source 加工成需要的，在Adapter类中提供加工
  * @date 2020/12/8 10:23
  */
 public class Main {
