f1c1d222db7b24a31bca9b46bb9fe5e5cce31ad2
chengjunchao <jccheng@zenking.cc>
2020-12-30 17:18:14 +0800

    KMP 匹配字符串

--- src/com/jikong/Subject28.java
+++ src/com/jikong/Subject28.java
@@ -12,17 +12,32 @@ public class Subject28 {
         if (needle.length() == 0) {
             return 0;
         }
-        int m = haystack.length();
-        int n = needle.length();
-        for (int i = 0; i <= m - n; i++) {
-            int j;
-            for (j = 0; j < n; j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    break;
-                }
+        int[] next = new int[needle.length()];
+        //先组装 next
+        int j = 0;
+        for (int i = 2; i < needle.length(); i++) {
+            while (j != 0 && needle.charAt(j) != needle.charAt(i - 1)) {
+                //从next[i+1]的求解回溯到 next[j]
+                j = next[j];
             }
-            if (j == n) {
-                return i;
+            if (needle.charAt(j) == needle.charAt(i - 1)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        j = 0;
+        //主循环，遍历主串字符
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                //遇到坏字符时，查询next数组并改变模式串的起点
+                j = next[j];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                //匹配成功，返回下标
+                return i - needle.length() + 1;
             }
         }
         return -1;
@@ -30,7 +45,7 @@ public class Subject28 {
 
     public static void main(String[] args) {
         //haystack = "hello", needle = "ll" 2 , haystack = "aaaaa", needle = "bba" -1
-        String haystack = "aa", needle = "aaa";
+        String haystack = "hello", needle = "ll";
         Subject28 subject28 = new Subject28();
         System.out.println(subject28.strStr(haystack, needle));
     }
