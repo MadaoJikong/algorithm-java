107dee54e7dbaaf6fcb6d577152ef1d58ae67688
chengjunchao <jccheng@zenking.cc>
2021-01-04 18:09:38 +0800

    107. 二叉树的层序遍历 II

---
+++ src/com/jikong/Subject107.java
@@ -0,0 +1,62 @@
+package com.jikong;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chengjunchao
+ * @title: Subject107
+ * @projectName algorithm-java
+ * @description: 107. 二叉树的层序遍历 II
+ * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * @date 2021/1/4 17:04
+ */
+public class Subject107 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        f(root);
+        return null;
+    }
+
+    private List<Integer> f(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        List<Integer> l = f(root.left);
+        List<Integer> r = f(root.right);
+        List<Integer> list = new ArrayList<>();
+        if (l != null) {
+            list.add(root.left.val);
+        }
+        if (r != null) {
+            list.add(root.right.val);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Subject107 subject107 = new Subject107();
+        TreeNode treeNode3 = subject107.new TreeNode(3);
+        TreeNode treeNode9 = subject107.new TreeNode(9);
+        TreeNode treeNode20 = subject107.new TreeNode(20);
+        TreeNode treeNode15 = subject107.new TreeNode(15);
+        TreeNode treeNode7 = subject107.new TreeNode(7);
+        treeNode3.left = treeNode9;
+        treeNode3.right = treeNode20;
+        treeNode20.left = treeNode15;
+        treeNode20.right = treeNode7;
+
+        List<List<Integer>> list = subject107.levelOrderBottom(treeNode3);
+        System.out.println(list);
+    }
+}
