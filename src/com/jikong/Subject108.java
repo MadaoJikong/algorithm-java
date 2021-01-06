1e96bf8a93e9dd86c2ed082ab66eaf724e7c686a
chengjunchao <jccheng@zenking.cc>
2021-01-06 10:03:09 +0800

    108. 将有序数组转换为二叉搜索树

--- src/com/jikong/Subject108.java
+++ src/com/jikong/Subject108.java
@@ -23,18 +23,15 @@ public class Subject108 {
         return helper(nums, 0, nums.length - 1);
     }
 
-    public TreeNode helper(int[] nums, int left, int right) {
+    private TreeNode helper(int[] nums, int left, int right) {
         if (left > right) {
             return null;
         }
-
-        // 总是选择中间位置左边的数字作为根节点
         int mid = (left + right) / 2;
-
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = helper(nums, left, mid - 1);
-        root.right = helper(nums, mid + 1, right);
-        return root;
+        TreeNode treeNode = new TreeNode(nums[mid]);
+        treeNode.left = helper(nums, left, mid - 1);
+        treeNode.right = helper(nums, mid + 1, right);
+        return treeNode;
     }
 
     public static void main(String[] args) {
