fccdf0cfa77c491ec952f8f9dde71315080191d5
chengjunchao <jccheng@zenking.cc>
2021-01-12 10:35:09 +0800

    226. 翻转二叉树  深度递归实现和广度队列实现

--- src/com/jikong/Subject226.java
+++ src/com/jikong/Subject226.java
@@ -1,5 +1,8 @@
 package com.jikong;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author chengjunchao
  * @title: Subject226
@@ -19,12 +22,57 @@ public class Subject226 {
     }
 
     /**
+     * 深度遍历 递归实现
+     *
      * @param root
      * @return
      */
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-        return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /**
+     * 广度遍历  队列实现
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                //循环体内 顺序获取本层
+                TreeNode treeNode = queue.poll();
+                TreeNode temp = treeNode.left;
+                treeNode.left = treeNode.right;
+                treeNode.right = temp;
+                //添加下层
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+        }
+        return root;
     }
 
     public static void main(String[] args) {
@@ -42,6 +90,7 @@ public class Subject226 {
         treeNode2.right = treeNode3;
         treeNode7.left = treeNode6;
         treeNode7.right = treeNode9;
-
+        TreeNode treeNode = subject226.invertTreeBFS(treeNode4);
+        System.out.println();
     }
 }
