aa02a4987104951cd5aab4ca72bf175bf0ace136
chengjunchao <jccheng@zenking.cc>
2021-01-13 18:10:56 +0800

    121. 买卖股票的最佳时机

---
+++ src/com/jikong/Subject121.java
@@ -0,0 +1,25 @@
+package com.jikong;
+
+/**
+ * @author chengjunchao
+ * @title: Subject121
+ * @projectName algorithm-java
+ * @description: 121. 买卖股票的最佳时机
+ * @date 2021/1/13 17:58
+ */
+public class Subject121 {
+    public int maxProfit(int[] prices) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Subject121 subject121 = new Subject121();
+        //5
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+//        //0
+//        int[] prices = new int[]{7, 6, 4, 3, 1};
+        int result = subject121.maxProfit(prices);
+        System.out.println(result);
+    }
+}
