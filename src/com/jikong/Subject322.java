2b97b522269d9c4f144e2dee97661e7f2ebeecb5
chengjunchao <jccheng@zenking.cc>
2021-01-20 11:45:34 +0800

    322.零钱兑换

--- src/com/jikong/Subject322.java
+++ src/com/jikong/Subject322.java
@@ -1,5 +1,7 @@
 package com.jikong;
 
+import java.util.Arrays;
+
 /**
  * @author chengjunchao
  * @title: Subject32
@@ -13,16 +15,16 @@ public class Subject322 {
             return 0;
         }
         int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            int res = Integer.MAX_VALUE;
             for (int j = 0; j < coins.length; j++) {
                 if (i - coins[j] >= 0) {
-                
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        return -1;
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 
     public static void main(String[] args) {
