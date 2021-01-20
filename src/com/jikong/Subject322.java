93e2a66cb60dcd221a554331d33911148fd46e9f
chengjunchao <jccheng@zenking.cc>
2021-01-20 18:00:34 +0800

    322. 零钱兑换

--- src/com/jikong/Subject322.java
+++ src/com/jikong/Subject322.java
@@ -15,7 +15,7 @@ public class Subject322 {
             return 0;
         }
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
@@ -24,7 +24,7 @@ public class Subject322 {
                 }
             }
         }
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 
     public static void main(String[] args) {
