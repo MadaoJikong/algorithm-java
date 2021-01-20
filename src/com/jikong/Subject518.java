462b588f50896dea036bf62373fbecf20e0a4205
chengjunchao <jccheng@zenking.cc>
2021-01-20 18:09:57 +0800

    518. 零钱兑换 II

--- src/com/jikong/Subject518.java
+++ src/com/jikong/Subject518.java
@@ -9,12 +9,18 @@ package com.jikong;
  */
 public class Subject518 {
     public int change(int amount, int[] coins) {
+        if (coins.length == 0) {
+            return 0;
+        }
+        //两个状态，使用几个coins，凑多少钱。coins 前几个，可以凑够 多少钱。能有dp[i][j] 个方式
+        int[][] dp = new int[coins.length + 1][amount + 1];
 
         return 1;
     }
 
     public static void main(String[] args) {
         Subject518 subject518 = new Subject518();
+        //输出4
         int amount = 5;
         int[] coins = {1, 2, 5};
         int result = subject518.change(amount, coins);
