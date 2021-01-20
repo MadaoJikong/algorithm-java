ad71bbf4369d3d0156e4308fd631b641b9569ede
chengjunchao <jccheng@zenking.cc>
2021-01-20 15:59:40 +0800

    518. 零钱兑换 II

---
+++ src/com/jikong/Subject518.java
@@ -0,0 +1,23 @@
+package com.jikong;
+
+/**
+ * @author chengjunchao
+ * @title: Subject518
+ * @projectName algorithm-java
+ * @description: 518. 零钱兑换 II
+ * @date 2021/1/20 15:57
+ */
+public class Subject518 {
+    public int change(int amount, int[] coins) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Subject518 subject518 = new Subject518();
+        int amount = 5;
+        int[] coins = {1, 2, 5};
+        int result = subject518.change(amount, coins);
+        System.out.println(result);
+    }
+}
