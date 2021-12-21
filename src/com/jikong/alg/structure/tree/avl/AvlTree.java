2e7da62dd2a3023ef808ccb9e4c7002ae21a592b
chengjunchao <121777922@qq.com>
2021-12-21 17:39:10 +0800

    avl树

--- src/com/jikong/alg/structure/tree/avl/AvlTree.java
+++ src/com/jikong/alg/structure/tree/avl/AvlTree.java
@@ -120,26 +120,49 @@ public class AvlTree {
         preOrder(root);
     }
 
-    private TreeNode insert(TreeNode tree, int key, int height) {
+    private TreeNode insert(TreeNode tree, int key) {
         if (tree == null) {
             // 新建节点
             tree = new TreeNode(key, null, null);
-            tree.height = height;
         } else {
             int com = compare(key, tree.key);
             if (com < 0) {
-                tree.left = insert(tree.left, key, height + 1);
+                tree.left = insert(tree.left, key);
+                tree.height = max(tree.height, tree.left.height + 1);
+//                if (height(tree.left) - height(tree.right) == 2) {
+//                    if (compare(key, tree.left.key) < 0) {
+//                        tree = leftLeftRotation(tree);
+//                    } else {
+//                        tree = leftRightRotation(tree);
+//                    }
+//                }
             } else if (com > 0) {
-                tree.right = insert(tree.right, key, height + 1);
+                tree.right = insert(tree.right, key);
+                tree.height = max(tree.height, tree.right.height + 1);
+//                if (height(tree.right) - height(tree.left) == 2) {
+//                    if (compare(tree.right.key, key) > 0) {
+//                        tree = rightRightRotation(tree);
+//                    } else {
+//                        tree = rightLeftRotation(tree);
+//                    }
+//                }
             } else {
-                tree.right = insert(tree.right, key, height + 1);
+                tree.right = insert(tree.right, key);
+                tree.height = max(tree.height, tree.right.height + 1);
+//                if (height(tree.right) - height(tree.left) == 2) {
+//                    if (compare(tree.right.key, key) > 0) {
+//                        tree = rightRightRotation(tree);
+//                    } else {
+//                        tree = rightLeftRotation(tree);
+//                    }
+//                }
             }
         }
         return tree;
     }
 
     public void insert(int key) {
-        root = insert(root, key, 1);
+        root = insert(root, key);
     }
 
     /**
@@ -168,7 +191,54 @@ public class AvlTree {
     }
 
 
-    private TreeNode leftLeftRotation(TreeNode treeNode) {
-        return null;
+    private TreeNode leftLeftRotation(TreeNode k2) {
+        TreeNode k1;
+
+        k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        //关于高度，树的高度即为最大层次。
+        //即空的二叉树的高度是0，非空树的高度从1计数，等于它的最大层次(根的层次为1，根的子节点为第2层，依次类推)。
+        k2.height = max(height(k2.left), height(k2.right)) + 1;
+        k1.height = max(height(k1.left), k2.height) + 1;//左右比较
+
+        return k1;
     }
+
+    private TreeNode rightRightRotation(TreeNode k1) {
+        TreeNode k2;
+
+        k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+
+        k1.height = max(height(k1.left), height(k1.right)) + 1;
+        k2.height = max(height(k2.right), k1.height) + 1;
+
+        return k2;
+    }
+
+    /*
+     * LR：左右对应的情况(左双旋转)。--对应RR-LL
+     *(插入节点是左子树的右边节点)
+     * 返回值：旋转后的根节点
+     */
+    private TreeNode leftRightRotation(TreeNode k3) {
+
+        k3.left = rightRightRotation(k3.left);
+
+        return leftLeftRotation(k3);
+    }
+
+    /*
+     * RL：右左对应的情况(右双旋转)。对应 LL-RR
+     *(插入节点是右子树的左边节点)
+     * 返回值：旋转后的根节点
+     */
+    private TreeNode rightLeftRotation(TreeNode k1) {
+        k1.right = leftLeftRotation(k1.right);
+
+        return rightRightRotation(k1);
+    }
+
 }
