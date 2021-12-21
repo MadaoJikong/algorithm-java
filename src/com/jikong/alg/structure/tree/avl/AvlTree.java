0c8bc9b1ae3f9f52cae7d01db9ee9a48ec8c162e
chengjunchao <121777922@qq.com>
2021-12-21 10:31:34 +0800

    avl树

--- src/com/jikong/alg/structure/tree/avl/AvlTree.java
+++ src/com/jikong/alg/structure/tree/avl/AvlTree.java
@@ -1,5 +1,8 @@
 package com.jikong.alg.structure.tree.avl;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * avl树
  *
@@ -69,21 +72,103 @@ public class AvlTree {
         }
     }
 
+    /**
+     * 广度遍历
+     */
+    public void bfs() {
+        b(root);
+    }
+
+    /**
+     * 广度遍历
+     *
+     * @param root
+     */
+    private void b(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode != null) {
+                    System.out.print(treeNode.key + ":" + treeNode.height + " ");
+                } else {
+                    System.out.print("null ");
+                }
+                if (treeNode != null) {
+                    if (treeNode.left != null) {
+                        queue.offer(treeNode.left);
+                    } else {
+                        queue.offer(null);
+                    }
+                }
+                if (treeNode != null) {
+                    if (treeNode.right != null) {
+                        queue.offer(treeNode.right);
+                    } else {
+                        queue.offer(null);
+                    }
+                }
+            }
+            System.out.println();
+
+        }
+    }
+
     public void preOrder() {
         preOrder(root);
     }
 
-    private TreeNode insert(TreeNode tree, int key) {
+    private TreeNode insert(TreeNode tree, int key, int height) {
         if (tree == null) {
             // 新建节点
             tree = new TreeNode(key, null, null);
+            tree.height = height;
         } else {
             int com = compare(key, tree.key);
+            if (com < 0) {
+                tree.left = insert(tree.left, key, height + 1);
+            } else if (com > 0) {
+                tree.right = insert(tree.right, key, height + 1);
+            } else {
+                tree.right = insert(tree.right, key, height + 1);
+            }
         }
         return tree;
     }
 
     public void insert(int key) {
-        root = insert(root, key);
+        root = insert(root, key, 1);
+    }
+
+    /**
+     * 获取某一结点的高度
+     *
+     * @return
+     */
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    /**
+     * 获取节点的平衡因子
+     *
+     * @param node
+     * @return
+     */
+    private int getBalanceFactor(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+
+    private TreeNode leftLeftRotation(TreeNode treeNode) {
+        return null;
     }
 }
--- src/com/jikong/alg/structure/tree/avl/Main.java
+++ src/com/jikong/alg/structure/tree/avl/Main.java
@@ -8,8 +8,16 @@ package com.jikong.alg.structure.tree.avl;
  * @date 2021/12/17 17:23
  */
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         AvlTree avlTree = new AvlTree();
+        avlTree.insert(10);
+        avlTree.insert(1);
+        avlTree.insert(2);
+        avlTree.insert(3);
+        avlTree.insert(11);
+        avlTree.insert(10);
+        avlTree.insert(14);
 
+        avlTree.bfs();
     }
 }
