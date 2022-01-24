a577d05a33510bf50430b8519dd6672bb9affd59
chengjunchao <121777922@qq.com>
2022-01-24 18:30:57 +0800

    图 最短路径优先

--- src/com/jikong/alg/structure/graph/list/GraphAdjList.java
+++ src/com/jikong/alg/structure/graph/list/GraphAdjList.java
@@ -1,5 +1,8 @@
 package com.jikong.alg.structure.graph.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 邻接表
  *
@@ -37,5 +40,60 @@ public class GraphAdjList {
                 edgeNode = edgeNode.next;
             }
         }
+        return -1;
+    }
+
+    /**
+     * 获取临近点权重
+     *
+     * @param from
+     * @param to
+     * @return
+     */
+    public int weight(int from, int to) {
+        VexNode vexNode = vexNodes[from];
+        EdgeNode edgeNode = vexNode.edgeNode;
+        while (edgeNode != null) {
+            if (edgeNode.id == to) {
+                return edgeNode.weight;
+            }
+            edgeNode = edgeNode.next;
+        }
+        return -1;
+    }
+
+    /**
+     * 获取临近点
+     *
+     * @param s
+     * @return
+     */
+    List<Integer> adj(int s) {
+        List<Integer> list = new ArrayList<>();
+        VexNode vexNode = vexNodes[s];
+        EdgeNode edgeNode = vexNode.edgeNode;
+        while (edgeNode != null) {
+            list.add(edgeNode.id);
+            edgeNode = edgeNode.next;
+        }
+        return list;
+    }
+
+    /**
+     * 深度优先
+     */
+    public void depthFirstSearch() {
+
+    }
+
+    private void depthFirstSearch(int v) {
+
+    }
+
+    /**
+     * 广度优先
+     */
+    public void breadFirstSearch() {
+
     }
 }
